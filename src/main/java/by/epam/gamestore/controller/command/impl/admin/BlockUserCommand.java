d71918093d546bf962ead344b106e03c9c9253d1
Ivliev Vitaly <vitalik.ivliev.2013@mail.ru>
2022-02-17 19:43:31 +0300

    add command

--- src/main/java/by/epam/gamestore/controller/command/impl/admin/BlockUserCommand.java
+++ src/main/java/by/epam/gamestore/controller/command/impl/admin/BlockUserCommand.java
@@ -6,6 +6,9 @@ import by.epam.gamestore.entity.User;
 import by.epam.gamestore.exception.ServiceException;
 import by.epam.gamestore.service.UserService;
 import by.epam.gamestore.service.impl.UserServiceImpl;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
@@ -13,15 +16,17 @@ import java.util.Optional;
 import static by.epam.gamestore.controller.command.ConstantParameter.*;
 
 public class BlockUserCommand implements Command {
+    private static final Logger logger = LogManager.getLogger();
     private UserService userService = UserServiceImpl.getInstance();
     @Override
     public CommandResponse execute(HttpServletRequest request) {
-        Optional<User> optionalUser = Optional.empty();
+        Optional<User> optionalUser;
         int user_id = Integer.parseInt(request.getParameter(USER_ID));
         try {
             optionalUser = userService.findUserById(user_id);
         } catch (ServiceException e) {
-            e.printStackTrace();
+            logger.log(Level.ERROR,e.getMessage());
+            return new CommandResponse(ERROR,ERROR_STATUS_500);
         }
         if(optionalUser.isPresent()){
             User user = optionalUser.get();
